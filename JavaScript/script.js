2b62feb1b83feae058b9b644fd94f43596430f18
F-Z <46107001+k-fatimaezzahra@users.noreply.github.com>
2021-04-27 03:22:34 +0000

    set background and ffullpage_scroll

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+
+const el = document.querySelector(".move");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
