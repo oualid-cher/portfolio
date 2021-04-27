50107527ec963cc5aa71517a41d2f0cf07e711a7
oualid cherkaoui <ahmedoualidcherkaoui@gmail.com>
2021-04-27 16:00:11 +0000

    last push

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,6 @@
-
 const el = document.querySelector(".move");
 
 el.addEventListener("mousemove", (e) => {
-  el.style.backgroundPositionX = -e.offsetX + "px";
-  el.style.backgroundPositionY = -e.offsetY + "px";
+    el.style.backgroundPositionX = -e.offsetX + "px";
+    el.style.backgroundPositionY = -e.offsetY + "px";
 });
